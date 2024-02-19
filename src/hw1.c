397ca41635384dc3994498735968e22921b9ab74
andy3200 <149091611+andy3200@users.noreply.github.com>
2024-02-19 00:50:44 +0000

    implemented part 3

--- src/hw1.c
+++ src/hw1.c
@@ -144,11 +144,22 @@ unsigned int compute_checksum_sf(unsigned char packet[])
 }
 
 unsigned int reconstruct_array_sf(unsigned char *packets[], unsigned int packets_len, int *array, unsigned int array_len) {
-    (void)packets;
-    (void)packets_len;
-    (void)array;
-    (void)array_len;
-    return -1;
+    size_t packets_num = sizeof(packets) / sizeof(packets[0]);
+    unsigned int insert_count = 0; 
+    for(size_t x = 0; x <= packets_num-1; x++){
+        get_frag_offset(packets[x]); //get the fragment offset 
+        unsigned int start_index = (fragment_offset / (int) 4); //the starting index at the array to be put in 
+        get_checksum(packets[x]); //get the original checksum of the current packet 
+        unsigned int calculated_checksum = compute_checksum_sf(packets[x]);
+        if((checksum == calculated_checksum) && (start_index <= array_len-1)){// start putting in stuff into the array 
+            get_payload(packets[x]);
+            for(size_t y = start_index, z = 0; y <= array_len-1; y++, z++){
+                array[y] = payload[x];
+                insert_count++; 
+            }
+        }
+    }
+    return insert_count;
 }
 
 unsigned int packetize_array_sf(int *array, unsigned int array_len, unsigned char *packets[], unsigned int packets_len,
